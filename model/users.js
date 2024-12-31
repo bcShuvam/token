c8606ad15d1902252c28fe9de2309119d1ac3924
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2024-12-31 17:16:31 +0545

    SB-NJ-4: Added field in user model and structured response in login

--- model/users.js
+++ model/users.js
@@ -4,7 +4,13 @@ const userSchema = mongoose.Schema(
   {
     username: { type: String, required: true },
     password: { type: String, required: true },
+    role: { type: String, required: true },
+    department: { type: String, required: true },
+    designation: { type: String, required: true },
+    email: { type: String, default: "" },
     number: { type: Number, default: "" },
+    address: { type: String, default: "" },
+    dob: { type: String, default: "" },
     isFirstLogin: { type: Boolean, default: true },
     reset: { type: Boolean, default: true },
   },
