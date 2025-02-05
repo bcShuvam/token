0456c86646cbaccf89609290ef2e648c29a122f5
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2025-02-05 17:06:05 +0545

    SB-NJ-13: created category route in get poc api

--- routes/poc.js
+++ routes/poc.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 const {
   getPOCs,
+  getPOCByCreatedByIdAndCategory,
   getPocCreatedById,
   createPOC,
   pocFollowUp,
 } = require("../controller/pocController");
 
 router.route("").get(getPOCs);
+router.route("/category").get(getPOCByCreatedByIdAndCategory);
 router.route("/createdBy").get(getPocCreatedById);
 router.route("/create").post(createPOC);
 router.route("/followup").post(pocFollowUp);
