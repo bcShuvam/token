7d34df548c174481b9b4fe2aa687f046fd997f41
Shuvam BC <81623225+bcShuvam@users.noreply.github.com>
2025-02-18 02:41:21 +0545

    SB-NJ-19: minor changes in testHelloController.js

--- controller/testHelloController.js
+++ controller/testHelloController.js
@@ -1,5 +1,9 @@
 const testHello = (req, res) => {
-  res.status(200).json({ message: "Hello, World! test successful" });
+  try {
+    res.status(200).json({ message: "Hello, World! test successful" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = testHello;
