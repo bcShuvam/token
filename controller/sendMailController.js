ed2285e01fe6d5f15e053e595bf04874f81c039f
Shuvam BC <bcshuvam@gmail.com>
2025-03-04 13:36:15 +0545

    SB-NJ-29

--- controller/sendMailController.js
+++ controller/sendMailController.js
@@ -4,21 +4,26 @@ const path = require("path");
 const rootDirName = path.join(__dirname, "..");
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  // host: "smtp.gmail.com",
+  host: process.env.GOLDENMAIL,
   port: 587,
-  secure: false,
+  port: 465,
+  secure: true,
   auth: {
-    user: process.env.USERMAIL,  // Correctly loading .env values
-    pass: process.env.APP_PASSWORD,
+    // user: process.env.USERMAIL,
+    // pass: process.env.APP_PASSWORD,
+    user: process.env.GOLDENMAIL,
+    pass: process.env.GOLDEN_APP_PASSWORD,
   },
 });
 
 const mailOptions = {
   from: {
     name: "Deskgoo",
-    address: process.env.USERMAIL,
+    // address: process.env.USERMAIL,
+    address: process.env.GOLDENMAIL,
   },
-  to: ["dhungelnimesh@gmail.com", "drscorpion4@gmail.com", "kshitijdahal456@gmail.com"],
+  to: ["dhungelnimesh@gmail.com", "drscorpion4@gmail.com", "kshitijdahal456@gmail.com", "jyotishahqwerty@gmail.com"],
   subject: "Sending Email through NodeMailer",
   text: "Hello from Deskgoo TEXT✔",
   html: "<b>Hello from Deskgoo HTML✔</b>",
@@ -38,8 +43,10 @@ const mailOptions = {
 
 const sendMail = async (req, res) => {
   try {
-    console.log(process.env.USERMAIL);
-    console.log(process.env.APP_PASSWORD);
+    // console.log(process.env.USERMAIL);
+    // console.log(process.env.APP_PASSWORD);
+    console.log(process.env.GOLDENMAIL);
+    console.log(process.env.GOLDEN_APP_PASSWORD);
     const info = await transporter.sendMail(mailOptions);
     console.log(`Email sent: ${info.messageId}`);
     return res.status(200).json({ message: "Mail sent successfully", mail: info });
