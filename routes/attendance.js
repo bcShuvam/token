5d7d8f0e0e99146bf7695718e5ca314223fb10e1
Shuvam BC <bcshuvam@gmail.com>
2025-06-16 12:04:58 +0545

    SB-NJ-44: Forgot password route

--- routes/attendance.js
+++ routes/attendance.js
@@ -7,9 +7,11 @@ const {
   getAllAttendanceByDate,
   exportAttendanceToCSV,
   downloadAttendanceReport,
+  downloadAttendanceReportById
 } = require("../controller/attendanceController");
 router.route("/userId").get(getAttendanceById).post(postAttendanceByID);
 router.route("/date").get(getAttendanceByIdAndDate);
+router.route("/download").get(downloadAttendanceReportById);
 router.route("/date/all").get(getAllAttendanceByDate);
 router.get('/export', exportAttendanceToCSV);
 router.get('/report', downloadAttendanceReport);
