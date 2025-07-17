e293de6ef3d7225b04f48c97afe63b8085bb2888
Shuvam BC <bcshuvam@gmail.com>
2025-07-17 17:09:49 +0545

    Attendance response modified

--- controller/attendanceController.js
+++ controller/attendanceController.js
@@ -189,7 +189,7 @@ const getAllAttendanceByDate = async (req, res) => {
           totalHoursWorked: filteredAttendance.length > 0 ? totalHours : 0,
           totalTime: filteredAttendance.length > 0 ? formattedTime : "00:00:00",
           checkInTime: filteredAttendance?.checkIn?.deviceInTime ?? filteredAttendance,
-          checkOutTime: filteredAttendance?.checkOut?.deviceOutTime ?? '',
+          checkOutTime: filteredAttendance?.checkOut ?? '',
           totalAttendance: filteredAttendance.length,
         };
       } else {
