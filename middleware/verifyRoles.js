2c023dd795095d0fbdbb48a2a3fde97f3b8819f0
Shuvam BC <bcshuvam@gmail.com>
2025-08-13 21:36:19 +0545

    SB-NJ-50: update referral approvalStatus

--- middleware/verifyRoles.js
+++ middleware/verifyRoles.js
@@ -1,15 +1,16 @@
-const verifyRoles = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!req?.roles) return res.sendStatus(401);
-    const rolesArray = [...allowedRoles];
-    console.log(rolesArray);
-    console.log(req.roles);
-    const result = req.roles
-      .map((role) => rolesArray.includes(role))
-      .find((val) => val === true);
-    if (!result) return res.sendStatus(401);
-    next();
-  };
+const verifyRole = (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req?.user?.role) {
+            return res.status(401).json({ message: "Unauthorized: No role found" });
+        }
+
+        const rolesArray = [...allowedRoles];
+        if (!rolesArray.includes(req.user.role)) {
+            return res.status(403).json({ error: "Forbidden: Insufficient role" });
+        }
+
+        next();
+    };
 };
 
-module.exports = verifyRoles;
+module.exports = verifyRole;
