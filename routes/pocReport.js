e90d0f6eacb980c5f8147e693fb493f8a3ea6e57
Shuvam BC <bcshuvam@gmail.com>
2025-08-19 15:38:31 +0545

    SB-NJ-51: converting date and time to BS

--- routes/pocReport.js
+++ routes/pocReport.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { pocReport } = require('../controller/pocReportController');
+const { pocReport,pocReportCSV } = require('../controller/pocReportController');
 
 router.route('/').get(pocReport);
+router.route('/csv').get(pocReportCSV);
 
 module.exports = router;
